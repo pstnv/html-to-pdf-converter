a3d1049634e93c08c13b8e7b5477d1bd54e7c7f3
pstnv <pstnv@mail.ru>
2024-04-12 18:21:18 +0500

    fix: добавить next в middleware function errorHandlerMiddleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,6 @@
 import StatusCodes from "http-status-codes";
 
-const errorHandlerMiddleware = (err, req, res) => {
+const errorHandlerMiddleware = (err, req, res, next) => {
     let customError = {
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || "Что-то пошло не так. Попробуйте позже",
