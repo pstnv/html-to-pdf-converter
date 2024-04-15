e9d57c22ab26b6e8dfed28b64ea12936e2b9a942
pstnv <pstnv@mail.ru>
2024-04-15 18:50:17 +0500

    refactor: добавить функцию проверки авторизации пользователя

---
+++ middleware/authentication.js
@@ -0,0 +1,22 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import { UnauthenticatedError } from "../errors/index.js";
+
+const auth = async (req, res, next) => {
+    // проверяем заголовки headers
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        throw new UnauthenticatedError("Авторизация не пройдена");
+    }
+    const token = authHeader.split(" ").pop();
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // прикрепляем юзера к маршруту conversions
+        req.user = { userId: payload.userId, name: payload.name };
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError("Авторизация не пройдена");
+    }
+};
+
+export default auth;
