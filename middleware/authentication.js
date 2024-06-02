572a506ec4b3580cdf01628fa19cbd4e5de036d9
pstnv <pstnv@mail.ru>
2024-06-02 13:23:36 +0500

    refactor: переписать функцию checkAuth с использованием двух токенов (accessToken, refreshToken)

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,6 +4,7 @@ import { isTokenValid } from "../utils/index.js";
 import Token from "../models/Token.js";
 import { attachCookiesToResponse } from "../utils/index.js";
 
+// функция аутентификации используется на тех маршрутах, где авторизация обязательна
 const authenticateUser = async (req, res, next) => {
     const { accessToken, refreshToken } = req.signedCookies;
     try {
@@ -40,19 +41,39 @@ const authenticateUser = async (req, res, next) => {
     }
 };
 
+// функция проверки аутентификации используется на тех маршрутах, где авторизация не обязательна
 const checkAuthentication = async (req, res, next) => {
-    const token = req.signedCookies.token;
-    // проверяем куки на наличие токена
-    // если их нет, продолжаем конвертацию без авторизации
-    if (!token) {
-        return next();
-    }
-    // пытаемся авторизоваться
+    const { accessToken, refreshToken } = req.signedCookies;
     try {
-        // проверяем действующий токен
-        const { name, userId } = isTokenValid(token);
-        // прикрепляем юзера к запросу (без пароля!)
-        req.user = { name, userId };
+        // проверяем, есть ли действующий токен доступа accessToken для коротких сессий
+        if (accessToken) {
+            const payload = isTokenValid(accessToken);
+            // если токен существует и действителен, записываем в запрос пользователя
+            req.user = payload.user;
+            return next();
+        }
+
+        // если срок действия accessToken истек, обращаемся к refreshToken
+        // проверяем, есть ли действующий токен refreshToken для длинных сессий
+        const payload = isTokenValid(refreshToken);
+        const existingToken = await Token.findOne({
+            user: payload.user.userId,
+            refreshToken: payload.refreshToken,
+        });
+        // если токен не существует или не является действующим,
+        // не выбрасываем ошибку - продолжаем конвертацию без авторизации
+        if (!existingToken || !existingToken?.isValid) {
+            return next();
+        }
+        // если токен существует и действителен, записываем в запрос пользователя
+        req.user = payload.user;
+        // прикрепляем куки (создадим новый accessToken, обновим срок действия refreshToken)
+        attachCookiesToResponse({
+            res,
+            user: payload.user,
+            refreshToken: existingToken.refreshToken,
+        });
+        return next();
     } catch (error) {
         // выводим в консоль ошибку, но не выбрасываем, т.к. авторизация не обязательна
         console.log(error.message);
